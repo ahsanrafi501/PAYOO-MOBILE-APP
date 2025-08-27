1b09fa7314767f1a04d3f105c2e5d62fef6758a2
Ahsan Habib <habib23105341021@diu.edu.bd>
2025-08-28 04:45:07 +0600

    Cash Out Done

--- home.js
+++ home.js
@@ -1,4 +1,7 @@
 const pinNumber = 1234;
+
+// Toggling Feature
+
 document.getElementById("addMoneyParent").style.display = "none";
 document.getElementById("cashOutParent").style.display = "none";
 document.getElementById("addMoneyBtnParent").addEventListener("click", function (e) {
@@ -15,15 +18,17 @@ document.getElementById("cashOutBtnParent").addEventListener("click", function (
 
 
 
+// Add Money Feature
+
 document.getElementById('addMoneyBtn').addEventListener("click", function (e) {
     e.preventDefault();
 
-    selectedBank = document.getElementById("selectBank").value;
-    givenAccountNumber = document.getElementById("bankAccountNumber").value;
-    givenAmount = parseInt(document.getElementById("addAmount").value);
-    givenPinNumber = document.getElementById("pinNumber").value;
+    const selectedBank = document.getElementById("selectBank").value;
+    const givenAccountNumber = document.getElementById("bankAccountNumber").value;
+    const givenAmount = parseInt(document.getElementById("addAmount").value);
+    const givenPinNumber = document.getElementById("pinNumber").value;
 
-    availableBalance = parseInt(document.getElementById("totalAmount").innerText);
+    const availableBalance = parseInt(document.getElementById("totalAmount").innerText);
 
 
     if (givenAccountNumber.length < 11) {
@@ -36,7 +41,43 @@ document.getElementById('addMoneyBtn').addEventListener("click", function (e) {
         return;
     }
 
-    totalBalance = availableBalance + givenAmount;
+    const totalBalance = availableBalance + givenAmount;
 
     document.getElementById("totalAmount").innerText = totalBalance;
+})
+
+
+
+// Cash Out feature
+
+document.getElementById("withdrawBtn").addEventListener("click", function (e) {
+    e.preventDefault();
+    const withdrawAgentNumber = document.getElementById("withdrawAgentNumber").value;
+
+    const withdrawAmount = parseInt(document.getElementById("withdrawAddAmount").value);
+
+    const givenPinNumber = document.getElementById("withdrawPinNumber").value;
+const availableBalance = parseInt(document.getElementById("totalAmount").innerText);
+
+    if (withdrawAgentNumber.length < 11) {
+        alert("Enter Valid Agent Number");
+        return;
+    }
+
+    if (givenPinNumber != pinNumber) {
+        alert("Enter Valid Pin Number");
+        return;
+    }
+
+    if(availableBalance > withdrawAmount){
+        
+    const totalBalance = availableBalance - withdrawAmount;
+    document.getElementById("totalAmount").innerText = totalBalance;
+    }
+
+    else{
+        alert("Invalid Amount");
+    }
+
+
 })
